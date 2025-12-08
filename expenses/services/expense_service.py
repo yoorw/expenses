17394d4f05e214017ffa664c26cc4221f6095f55
Ryan Yoo <yoorw@hotmail.com>
2025-12-08 16:49:59 -0600

    delete unused code

--- expenses/services/expense_service.py
+++ expenses/services/expense_service.py
@@ -1,39 +1,11 @@
 from decimal import Decimal
 
-# from expenses.models.expense import Expense
 from expenses.validators.expense_validator import (
     NameValidator,
     AmountValidator,
     DueDayValidator,
     IsFixedValidator
 )
-
-# def create_expense(
-#     name: str,
-#     amount: any,
-#     due_day: int,
-#     is_fixed: bool
-# ) -> Expense:
-#     """Create and return an Expense object after validating inputs."""
-    
-#     try:
-#         # Validate inputs
-#         NameValidator().validate(name)
-#         AmountValidator().validate(amount)
-#         DueDayValidator().validate(due_day)
-#         IsFixedValidator().validate(is_fixed)
-
-#     except ValueError as ve:
-#         # if any Validator fails, return error response
-#         return {"error": str(ve)}
-    
-#     # Create and return Expense object
-#     return Expense(
-#         name=name,
-#         amount=Decimal(str(amount)),
-#         due_day=due_day,
-#         is_fixed=is_fixed
-#     )
 
 def clean_name(name: str) -> str:
     """Clean and return the expense name."""

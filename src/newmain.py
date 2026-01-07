44c368d91c22e0db6eeef16630c52e71407b6ab1
Ryan Yoo <yoorw@hotmail.com>
2026-01-06 22:41:58 -0600

    newmain has improved logic for handling expense input

--- src/newmain.py
+++ src/newmain.py
@@ -16,18 +16,63 @@ def is_quit(field: str) -> bool:
         return True
     return False
 
+def request_field(input_prompt: str) -> str | None:
+    valid_field = False 
+
+    while not valid_field:
+        field = input(input_prompt).strip()
+
+        if is_quit(field):           # check for quit command first 
+            # break
+            return None
+
+        elif is_field_valid(field):
+            # valid_field = True
+            return field
+
 def main():
-    # expense name
-    valid_field = True 
+    expenses = []
+    add_expenses = True 
 
-    while valid_field:
-        name = input("Enter expense name: ").strip()
+    while add_expenses:
+        # expense name
+        name = request_field("Enter expense name: ")
+        if name is None:
+            break
 
-        if is_quit(name):           # check for quit command first 
-            valid_field = False 
+        # expense amouunt
+        amount = request_field("Enter expense amount: ")
+        if amount is None:
+            break
 
-        elif is_field_valid(name):
-            valid_field = False 
+        if name and amount:
+            expenses.append(
+                {
+                    "name": name,
+                    "amount": Decimal(amount)
+                }
+            )
+
+            print(f"Expense entered: {expenses[-1]}")
+
+        add_expense_response = input("Do you want to add another expense? (yes/no): ").strip().lower()
+        print(f"add_expense_response: {add_expense_response}")
+        if add_expense_response in ["yes", "y"]:
+            continue
+        elif add_expense_response in ["no", "n"]:
+            print(f"These are your expenses: \n {expenses}")
+            print("Exiting input.")
+            add_expenses = False
+        elif is_quit(add_expense_response):
+            add_expenses = False 
+        else:
+            print("Invalid Response. Exiting input.")
+            break
+
+
+            
+
+
 
 
 

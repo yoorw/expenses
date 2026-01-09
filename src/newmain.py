148afc92be4dd204c410d510f66176cbf65af196
Ryan Yoo <yoorw@hotmail.com>
2026-01-08 21:04:23 -0600

    create_expense() for expense fields. improve logic for adding each expense

--- src/newmain.py
+++ src/newmain.py
@@ -29,46 +29,59 @@ def request_field(input_prompt: str) -> str | None:
         elif is_field_valid(field):
             # valid_field = True
             return field
+        
+def create_expense() -> dict | None:        #TODO: return Expense object 
+        # expense name
+        name = request_field("Enter expense name: ")
+        if name is None:
+            return None
+
+        # expense amouunt
+        amount = request_field("Enter expense amount: ")
+        if amount is None:
+            return None
+
+        # due_date
+        due_date = request_field("Enter expense due date (1-31): ")
+        if due_date is None:
+            return None
+
+        # fixed_due_date 
+        fixed_due_date = request_field("Is the Expense Due Date a fixed date every month? (yes/no): ")
+        if fixed_due_date is None:
+            return None
+
+        is_enabled = request_field("Is the expense enabled? (yes/no): ")
+        if is_enabled is None:
+            return None
+        
+        if name and amount and due_date and fixed_due_date and is_enabled:
+            expense = {
+                "name": name,
+                "amount": Decimal(amount),
+                "due_date": due_date,
+                "fixed_due_date": fixed_due_date,
+                "is_enabled": is_enabled
+            }
+
+            print(f"Expense entered: {expense}")
+
+            return expense 
+
 
 def main():
     expenses = []
     add_expenses = True 
 
     while add_expenses:
-        # expense name
-        name = request_field("Enter expense name: ")
-        if name is None:
-            break
 
-        # expense amouunt
-        amount = request_field("Enter expense amount: ")
-        if amount is None:
-            break
+        expense = create_expense()
 
-        # due_date
-        due_date = request_field("Enter expense due date (1-31): ")
-        if due_date is None:
-            break
+        if expense:
+            expenses.append(expense)
+        else:
+            print("Expense not created.")
 
-        # fixed_due_date 
-        fixed_due_date = request_field("Is the Expense Due Date a fixed date every month? (yes/no): ")
-        if fixed_due_date is None:
-            break
-
-        is_enabled = request_field("Is the expense enabled? (yes/no): ")
-        if is_enabled is None:
-            break
-
-
-        if name and amount:
-            expenses.append(
-                {
-                    "name": name,
-                    "amount": Decimal(amount)
-                }
-            )
-
-            print(f"Expense entered: {expenses[-1]}")
 
         add_expense_response = input("Do you want to add another expense? (yes/no): ").strip().lower()
         print(f"add_expense_response: {add_expense_response}")
@@ -89,6 +102,7 @@ def main():
         else:
             print("Invalid Response. Exiting input.")
             break
+
 
 
 

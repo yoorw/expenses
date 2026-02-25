b3a0205dd4b608a7c8217afb73736fac21fa5d11
yoorw <37107949+yoorw@users.noreply.github.com>
2026-02-24 22:15:58 -0600

    Feature/console app (#4)
    * add expense validator class and unit tests
    
    * add expense validator, expense service, and unit tests
    
    * fixed and updated tests
    
    * added safety 2 decimal conversion within Expense object for amount property.
    
    * reformat get_valid_input() and add_expense() methods
    
    * add logic for is_fixed field in expense_service
    
    * rm unused code
    
    * added test_main
    
    * created newmain with simple logic for input & feedback loop
    
    * create src folder for newmain and tests folder for new tests
    
    * newmain has improved logic for handling expense input
    
    * handle amount as Decimal. output as formatted US currency
    
    * add remaining expense fields
    
    * create_expense() for expense fields. improve logic for adding each expense
    
    * create_expenses() from main menu
    
    * save_expenses() converts expense dictionary to json locally
    
    * save_expenses() to capture json directory and file name
    
    * add is_split field to expenses
    
    * add is_split field to returned expenses
    
    * add calculate_payment_amounts() and schedule_payments() fxns
    
    * add calculate_payment_split() and backtrack() algorithm
    
    * update main() to create_payment_plan
    
    * update create_payment_plan() outputs details for each payment schedule
    
    * create_expenses() returns expenses
    
    * create utils.py with decimal_to_currency() fxn
    
    * delete old code
    
    * rename src/newmain.py to src/main.py
    
    * set correct return for create_payment_plan() and correct input args for schedule_payments()
    
    * revert changes for expense.py
    
    * revert changes for unaffected files
    
    * evenly space out functions
    
    * ask user if expenses saved as file
    
    * rename create_payment_plan -> print_payment_plan. function only prints out payment plans
    
    * fix main() logic

--- main.py
+++
@@ -1,6 +0,0 @@
-from expenses.models.expense import Expense
-
-exp = Expense("Internet Bill", 59.99, due_day=15, is_fixed=True)
-print(exp)
-print(exp.amount)
-print(exp.to_dict())
---
+++ src/main.py
@@ -0,0 +1,226 @@
+import json
+from decimal import Decimal, ROUND_HALF_UP
+
+from utils.decimalencoder import DecimalEncoder
+from utils.utils import decimal_to_currency
+
+
+def is_field_valid(field: str) -> bool:
+    """Check if the input field is valid (not empty)."""
+    if not field or field == "":
+        print(" Input cannot be empty. Please try again.")
+        return False
+
+    print(f"Expense value entered: {field}")
+    return True
+
+
+def is_quit(field: str) -> bool:
+    """Check if the input field is 'quit'."""
+    if field.lower() == "quit" or field.lower() == "q":
+        print(" Exiting input.")
+        return True
+    return False
+
+
+def request_field(input_prompt: str) -> str | None:
+    valid_field = False 
+
+    while not valid_field:
+        field = input(input_prompt).strip()
+
+        if is_quit(field):           # check for quit command first 
+            # break
+            return None
+
+        elif is_field_valid(field):
+            # valid_field = True
+            return field
+
+
+def create_expense() -> dict | None:        #TODO: return Expense object 
+        # expense name
+        name = request_field("Enter expense name: ")
+        if name is None:
+            return None
+
+        # expense amouunt
+        amount = request_field("Enter expense amount: ")
+        if amount is None:
+            return None
+
+        # due_date
+        due_date = request_field("Enter expense due date (1-31): ")
+        if due_date is None:
+            return None
+
+        # fixed_due_date 
+        fixed_due_date = request_field("Is the Expense Due Date a fixed date every month? (yes/no): ")
+        if fixed_due_date is None:
+            return None
+
+        is_split = request_field("Can the payment be split (2 payments per month)? (yes/no): ")
+        if is_split is None:
+            return None
+        
+        is_active = request_field("Is the expense an active, on-going expense? (yes/no): ")
+        if is_active is None:
+            return None
+        
+        if name and amount and due_date and fixed_due_date and is_active:
+            expense = {
+                "name": name,
+                "amount": Decimal(amount),
+                "due_date": due_date,
+                "fixed_due_date": fixed_due_date,
+                "is_split": is_split,
+                "is_active": is_active
+            }
+
+            print(f"Expense entered: {expense}")
+
+            return expense 
+
+
+def create_expenses() -> list[dict]: 
+    expenses = []
+    add_expenses = True 
+
+    while add_expenses:
+
+        expense = create_expense()
+
+        if expense:
+            expenses.append(expense)
+        else:
+            print("Expense not created.")
+
+
+        add_expense_response = input("Do you want to add another expense? (yes/no): ").strip().lower()
+        print(f"add_expense_response: {add_expense_response}")
+        if add_expense_response in ["yes", "y"]:
+            continue
+        elif add_expense_response in ["no", "n"]:
+            # print(f"\nThese are your expenses: \n {expenses}")
+            print("\nThese are your expenses:\n")
+
+            for expense in expenses:
+                amount_formatted = expense['amount'].quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+                print(f"  - {expense['name']}: ${amount_formatted:,.2f}")
+
+            print("\nSaving expenses...")
+            save_expenses(expenses)
+
+            print("\nExiting input.")
+
+
+            add_expenses = False
+
+            return expenses
+
+        elif is_quit(add_expense_response):
+            add_expenses = False 
+        else:
+            print("Invalid Response. Exiting input.")
+            break
+
+
+def save_expenses(expenses: list) -> None:
+    """Save expenses to a file or database (not implemented)."""
+    # file_path = "/Volumes/swdev/swdev_docs/expenses/expenses.json"
+
+    save_expenses = request_field("Do you want to save the Expenses as a file? (yes/no):")
+
+    if save_expenses in ["yes", "y"]:
+        file_name = request_field("Enter the name of the expenses file to save (e.g., expenses.json): ")
+        file_path = request_field("Enter full path directory to save the expenses file (e.g., /path/to/): ")
+        file_dir = file_path + file_name
+        with open(file_dir, 'w') as f:
+            json.dump(expenses, f, cls=DecimalEncoder, indent=4)
+
+        print(f"Expenses saved to {file_dir}.")
+
+def calculate_payment_split(expenses: list[dict]) -> tuple[list[dict], list[dict]]:
+    """Calculate payment amounts based on expenses. Uses Partition Backtracking Algorithm"""
+    # Placeholder implementation
+    payment_plan = []
+
+    total_expense = sum(expense['amount'] for expense in expenses)
+    target = total_expense / 2
+
+    best_subset = []
+    best_diff = float('inf')
+
+    def backtrack(index: int, current_subset: list[dict], current_sum: Decimal):
+        nonlocal best_subset, best_diff
+
+        # best case: all expenses considered 
+        if index == len(expenses):
+            diff = abs(target - current_sum)
+            if diff < best_diff:
+                best_diff = diff
+                best_subset = list(current_subset)
+
+            return 
+        
+        # Choice 1: Include the current expense in Subset 1
+        current_subset.append(expenses[index])
+        backtrack(index + 1, current_subset, current_sum + expenses[index]['amount'])
+        current_subset.pop() # Undo choice (backtrack)
+
+        # Choice 2: Exclude current expense from Subset 1
+        backtrack(index + 1, current_subset, current_sum)
+
+    # Start recursion
+    backtrack(0, [], 0)
+
+    # Organize results into payment plans
+    subset_1 = best_subset 
+    subset_2 = list(expenses)
+    for expense in subset_1:
+        # Remove one expense from subset_2
+        if expense in subset_2:
+            subset_2.remove(expense)
+
+    return subset_1, subset_2
+
+
+def print_payment_plan(subset_1: list[dict], subset_2: list[dict]) -> None:
+    """Create a payment plan based on the expenses (not implemented)."""
+    totals = []
+    for idx, expenses in enumerate([subset_1, subset_2]):
+        total = sum(expense['amount'] for expense in expenses)
+        totals.append(total)
+
+        print(f"\nPayment Schedule {idx+1}:")
+        total_formatted = decimal_to_currency(total)
+        print(f"Total Amount: ${total_formatted:,.2f}")
+        print("Expenses:")
+        for expense in expenses:
+            for expense_field, expense_val in expense.items():
+                if expense_field == "amount":
+                    expense_val = decimal_to_currency(expense_val)
+
+                print(f" - {expense_field}: {expense_val}")
+
+
+    expense_diff = abs(totals[0] - totals[1])
+    print(f"\nDifference between schedules: ${decimal_to_currency(expense_diff):,.2f}\n")
+
+
+def schedule_payments(payment_plan_1: list[dict], payment_plan_2: list[dict]) -> None:
+    """Schedule payments based on the payment plan (not implemented)."""
+    # Placeholder implementation
+    pass
+
+
+def main():
+    expenses = create_expenses()
+    subset_1, subset_2 = calculate_payment_split(expenses)
+    print_payment_plan(subset_1, subset_2)
+    # schedule_payments(schedule_1, schedule_2)
+
+
+
+if __name__ == "__main__":
+    main()

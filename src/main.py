1065cf5f36d2fc0bf1acdf157ddec9354c3c729c
Ryan Yoo <yoorw@hotmail.com>
2026-04-05 10:11:51 -0500

    update main with updated expense object fields

--- src/main.py
+++ src/main.py
@@ -70,7 +70,7 @@ def create_expense() -> dict | None:        #TODO: return Expense object
         if name and amount and due_date and fixed_due_date and is_active:
             expense = {
                 "name": name,
-                "amount": Decimal(amount),
+                "amount": amount,
                 "due_date": due_date,
                 "fixed_due_date": fixed_due_date,
                 "is_split": is_split,
@@ -101,12 +101,10 @@ def create_expenses() -> list[dict]:
         if add_expense_response in ["yes", "y"]:
             continue
         elif add_expense_response in ["no", "n"]:
-            # print(f"\nThese are your expenses: \n {expenses}")
             print("\nThese are your expenses:\n")
 
             for expense in expenses:
-                amount_formatted = expense['amount'].quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-                print(f"  - {expense['name']}: ${amount_formatted:,.2f}")
+                print(f"  - {expense['name']}: ${expense['amount']:,.2f}")
 
             print("\nSaving expenses...")
             # save_expenses(expenses)       #TODO: improve logic to save expenses
@@ -214,18 +212,16 @@ def print_payment_plan(subset_1: list[dict], subset_2: list[dict]) -> None:
         for expense in expenses:
             print("-"*20)
             for expense_field, expense_val in expense.items():
-                # convert amount field to currency format
-                expense_amt = decimal_to_currency(expense['amount'])
-
+                # convert amount field to string format
                 if expense_field == "amount":
-                    expense_val = expense_amt.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+                    expense_val = f"${total_formatted:,.2f}"
 
                 print(f" - {expense_field}: {expense_val}")
 
             # if the expense can be split, show split amount
             if expense['is_split'] in ['yes', 'y']:
-                split_val = decimal_to_currency(expense['amount'] / 2)
-                print(f"   -> Split Amount for {expense['name']}: {split_val.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)}")
+                split_val = expense['amount'] / 2
+                print(f"   -> Split Amount for {expense['name']}: ${split_val:,.2f}")
 
 
     expense_diff = abs(totals[0] - totals[1])
